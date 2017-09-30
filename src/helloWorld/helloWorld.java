3a80e3d83a735b31f27f07136d85270daf5724c9
Ltejeda <the01game@yahoo.com>
2017-09-30 14:42:42 -0700

    Git Test Part B Commit

---
+++ src/helloWorld/helloWorld.java
@@ -0,0 +1,12 @@
+package helloWorld;
+
+public class helloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Luis Tejeda");
+		System.out.println("CS3337-1, Saturday, 2:00-6:00pm");
+
+	}
+
+}
